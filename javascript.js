c0dbfd4e1230b95aab42b275209d1b99e7a8d92f
hansegucker <joniweth@gmx.de>
2017-01-16 20:26:40 +0100

    Add a simple read-out function for all dates and its times

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function toggle(id){
+    text='<span class="glyphicon glyphicon-ok"></span> Ausgewählt<span class="glyphicon glyphicon-arrow-right"></&nbsp>';
+    if(document.getElementById('selectinfo').innerHTML==""){
+        document.getElementById('selectinfo').innerHTML=text;
+        document.getElementById('select').innerHTML='<span class="glyphicon glyphicon-plus-sign"></span> Auswählen';
+        
+    }else{
+
+    }
+
+}
--- selectevent.php
+++ selectevent.php
@@ -18,30 +18,52 @@
     <![endif]-->
   </head>
   <body style="margin: 5px;">
+      <div class="page-header" style="text-align: center;">
+          <h1>Ticket(s) auswählen</h1>
+      </div>
+      <div class="alert alert-info" role="alert"><b>HINWEIS:</b> Bitte wähle unten die Zeiten aus, für die du Tickets kaufen möchtest!</div>
+
+
       <?php
         include("zugriff.inc.php");
+        $sqltogetdates="SELECT * FROM `dates`";
+        $resultdates=mysqli_query($db,$sqltogetdates);
+        while($row=mysqli_fetch_assoc($resultdates)){
+            echo '<div class="panel panel-default"><div class="panel-heading"><h2 class="panel-title">';
+            $datum=$row['datum'];
+            $wochentag=$row['wochentag'];
+            echo $wochentag.', den '.$datum;
+            echo '</h2></div><div class="panel-body">';
 
+
+            $sqltemp="SELECT * FROM `times` WHERE `date`=".$row["id"];
+            $resulttemp=mysqli_query($db,$sqltemp);
+            echo '<ul class="list-group">';
+
+
+            while($row=mysqli_fetch_assoc($resulttemp)){
+
+                $timestart=$row['timestart'];
+                $timeend=$row['timeend'];
+                $booked=intval($row['booked']);
+                $id=$row['id'];
+
+                if($booked==1){
+                    echo '<li class="list-group-item list-group-item-danger">'.$timestart.' - '.$timeend.' Uhr <span class="glyphicon glyphicon-arrow-right"></span>&nbsp <b>Belegt</b></li>';
+                }else{
+                    echo '<li class="list-group-item list-group-item-success">'.$timestart.' - '.$timeend.' Uhr <span class="glyphicon glyphicon-arrow-right"></span>&nbsp <b>Frei</B> <span class="glyphicon glyphicon-arrow-right"></span> ';
+                    echo '&nbsp<div id="selectinfo"></div><button id="select" type="button" class="btn btn-success" name="'.$id.'"><span class="glyphicon glyphicon-plus-sign"></span> Auswählen</button>';
+                    echo '</li>';
+                }
+
+            }
+            echo '</ul>';
+
+            echo '</div></div>';
+        }
        ?>
 
-
-
-
-    <div class="page-header" style="text-align: center;">
-        <h1>Ticket(s) auswählen</h1>
-    </div>
-    <div class="alert alert-info" role="alert"><b>HINWEIS:</b> Bitte wähle unten die Zeiten aus, für die du Tickets kaufen möchtest!</div>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">Sa, den 09.01.2017</h3>
-        </div>
-    <div class="panel-body">
-        Panel-Inhalt
-    </div>
-</div>
-
-
-
-
+    <script src="javascript.js"></script>
     <!-- jQuery (wird für Bootstrap JavaScript-Plugins benötigt) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Binde alle kompilierten Plugins zusammen ein (wie hier unten) oder such dir einzelne Dateien nach Bedarf aus -->
